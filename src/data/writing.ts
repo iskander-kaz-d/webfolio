3eb8e3f8d425879f8d1f3bf6554ed5badbfebff1
iskander-kaz-d <dyussenovir@gmail.com>
2026-09-08 11:04:55 -0400

    Sep 8 morning

--- src/data/writing.ts
+++ src/data/writing.ts
@@ -11,12 +11,12 @@ export const writingEntries: WritingEntry[] = [
     url: "https://docs.google.com/document/d/1tE-2ueA8EivxKayVnDF0v8xwxzyqwd9q2AFb8qdYQbU/edit?usp=sharing",
   },
   {
-    title: "I.W.",
+    title: "I.W., 2026",
     description: "In the year 2101, Roy Irwine, a security advisor at the Saint Lagrange Huang Conglomerate, has to embark on a whalehunt in the merciless waters of a frigid planet Helena. His son Gilly, seeking to help his father, sneaks onto the ship, putting himself and Roy in a volatile situation where a compromise between kindness, violence, masculinity, and softness has to be made. In a world where might makes right and predictive technologies threaten indisputable domination, can a boy survive without strangling his own heart?   ",
     url: "https://docs.google.com/document/d/1ZUukRegwsE4CIk7RyZGYmMRdZnoD1dmmu16uDqyOW2I/edit?usp=sharing",
   },
   {
-    title: "Madhound",
+    title: "Madhound, 2026",
     description: "A visceral story about hate, violence and fear. Will the end of the world bring the end of hate or will it only feed into the cycle of violence more?  Coming soon.",
     url: "#",
   },
